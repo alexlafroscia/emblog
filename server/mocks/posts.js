f34d1b702289e8afbdb42d3fc64337eaf97a8e3e
Alex LaFroscia <alex@lafroscia.com>
2015-05-28 14:57:39 -0400

    Add mock endpoints for posts

---
+++ server/mocks/posts.js
@@ -0,0 +1,40 @@
+var posts = [
+  { id: 1, title: 'this is my title' },
+  { id: 2, title: 'this is my title' },
+  { id: 3, title: 'this is my title' }
+];
+
+module.exports = function(app) {
+  var express = require('express');
+  var postsRouter = express.Router();
+
+  postsRouter.get('/', function(req, res) {
+    res.send({
+      'posts': posts
+    });
+  });
+
+  postsRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  postsRouter.get('/:id', function(req, res) {
+    res.send({
+      'posts': posts[req.params.id - 1]
+    });
+  });
+
+  postsRouter.put('/:id', function(req, res) {
+    res.send({
+      'posts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  postsRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/posts', postsRouter);
+};
